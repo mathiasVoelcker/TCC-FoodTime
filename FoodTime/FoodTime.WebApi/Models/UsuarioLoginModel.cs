acd47058fe48c9248fd17b4f9517204d18f69c05
Mathias Voelcker <mathias_V94@hotmail.com>
2017-12-07 15:32:18 -0200

    REQ2<@mathiasVoelcker> criar model para login de usuario

---
+++ FoodTime/FoodTime.WebApi/Models/UsuarioLoginModel.cs
@@ -0,0 +1,24 @@
+﻿using FoodTime.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FoodTime.WebApi.Models
+{
+    public class UsuarioLoginModel
+    {
+        public UsuarioLoginModel() { }
+
+        public UsuarioLoginModel(Usuario usuario)
+        {
+            Email = usuario.Email;
+            Senha = usuario.Senha;
+        }
+
+        public string Email { get; set; }
+
+        public string Senha { get; set; }
+
+    }
+}
