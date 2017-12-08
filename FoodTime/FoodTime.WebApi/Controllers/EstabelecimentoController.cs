6f16e3a752f2b73e08975aa961a50792693c3d4b
Mathias <mathias_V94@hotmail.com>
2017-12-08 21:57:24 -0200

    REQ9<@mathiasVoelcker> criar controller de estabelecimento

---
+++ FoodTime/FoodTime.WebApi/Controllers/EstabelecimentoController.cs
@@ -0,0 +1,23 @@
+﻿using FoodTime.Infraestrutura;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FoodTime.WebApi.Controllers
+{
+    [AllowAnonymous]
+    [RoutePrefix("api/estabelecimento")]
+    public class EstabelecimentoController : ApiController
+    {
+        IFoodTimeContext context;
+
+        public EstabelecimentoController()
+        {
+            context = new FoodTimeContext();
+        }
+
+    }
+}
