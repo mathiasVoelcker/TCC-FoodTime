c85448b119455bb6c4c870d46fbcda5daf8d5a93
Vitor Staevie Ramos <vitor.ramos@cwi.com.br>
2017-12-08 16:26:10 -0200

    Removendo entidades que nao serao utilizadas.

---
+++ FoodTime/FoodTime.UnitTest/CategoriaTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FoodTime.Dominio.Entidades;
+
+namespace FoodTime.UnitTest
+{
+    [TestClass]
+    public class CategoriaTest
+    {
+        [TestMethod]
+        public void Testar_Inicializacao_De_Categoria()
+        {
+            Categoria categoria;
+            var descricao = "testeDescricao";
+
+            categoria = new Categoria(descricao);
+
+            Assert.IsTrue(categoria.Descricao == descricao);
+        }
+
+        [TestMethod]
+        public void Testar_Inicializacao_De_Categoria_Com_Descricao_Invalida()
+        {
+            Categoria categoria;
+            var descricao = "testeNome";
+
+            categoria = new Categoria(descricao);
+
+            Assert.IsTrue(categoria.Descricao == descricao);
+        }
+    }
+}
