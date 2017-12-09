3f58256ca2e870c27b688e9dfd64e16568df34aa
Vitor Staevie Ramos <vitor.ramos@cwi.com.br>
2017-12-09 04:17:50 -0200

    feat(EstabelecimentoTese) este

--- FoodTime/FoodTime.UnitTest/EstabelecimentoTest.cs
+++ FoodTime/FoodTime.UnitTest/EstabelecimentoTest.cs
@@ -63,6 +63,34 @@ namespace FoodTime.UnitTest
         }
 
         [TestMethod]
+        public void Testar_Validar_De_Estabelecimento_Com_Endereco_Nulo_Ou_Vazio()
+        {
+            Endereco endereco = null;
+            List<Categoria> listaCategorias = new List<Categoria>();
+            List<Foto> listaFotos = new List<Foto>();
+            Estabelecimento estabelecimento;
+            var nome = "nome teste";
+            var telefone = "telefone teste";
+            DateTime horaFechamento = new DateTime().Date;
+            DateTime horaAbertura = new DateTime().Date;
+            decimal precoMedio = 14.0M;
+            bool aprovado = true;
+
+            estabelecimento = new Estabelecimento(nome, telefone, endereco, listaCategorias, horaAbertura, horaFechamento, precoMedio, listaFotos, aprovado);
+
+            Assert.IsTrue(estabelecimento.Nome == nome);
+            Assert.IsTrue(estabelecimento.Telefone == telefone);
+            Assert.IsTrue(estabelecimento.Categorias == listaCategorias);
+            Assert.IsTrue(estabelecimento.HorarioAbertura == horaAbertura);
+            Assert.IsTrue(estabelecimento.HorarioFechamento == horaFechamento);
+            Assert.IsTrue(estabelecimento.PrecoMedio == precoMedio);
+            Assert.IsTrue(estabelecimento.Fotos == listaFotos);
+            Assert.IsTrue(estabelecimento.Aprovado == aprovado);
+            Assert.IsTrue(estabelecimento.ValidarEntrada().Count == 1);
+            Assert.IsTrue(estabelecimento.ValidarEntrada().Contains("Deve conter um Endereço atrelado ao Estabelecimento."));
+        }
+
+        [TestMethod]
         public void Testar_Validar_De_Estabelecimento_Com_Nome_Maior()
         {
             Endereco endereco = new Endereco("rua teste", "numero teste", "", "", "Centro", "Porto Alegre", "Rio Grande do Sul", "96508060", 15.0M, 15.0M);
@@ -88,6 +116,233 @@ namespace FoodTime.UnitTest
             Assert.IsTrue(estabelecimento.Aprovado == aprovado);
             Assert.IsTrue(estabelecimento.ValidarEntrada().Count == 1);
             Assert.IsTrue(estabelecimento.ValidarEntrada().Contains("Tamanho máximo 100 caracteres."));
+        }
+
+        [TestMethod]
+        public void Testar_Validar_De_Estabelecimento_Com_Telefone_Nulo_Ou_Vazio()
+        {
+            Endereco endereco = new Endereco("rua teste", "numero teste", "", "", "Centro", "Porto Alegre", "Rio Grande do Sul", "96508060", 15.0M, 15.0M);
+            List<Categoria> listaCategorias = new List<Categoria>();
+            List<Foto> listaFotos = new List<Foto>();
+            Estabelecimento estabelecimento;
+            var nome = "nome teste";
+            var telefone = "";
+            DateTime horaFechamento = new DateTime().Date;
+            DateTime horaAbertura = new DateTime().Date;
+            decimal precoMedio = 14.0M;
+            bool aprovado = true;
+
+            estabelecimento = new Estabelecimento(nome, telefone, endereco, listaCategorias, horaAbertura, horaFechamento, precoMedio, listaFotos, aprovado);
+
+            Assert.IsTrue(estabelecimento.Nome == nome);
+            Assert.IsTrue(estabelecimento.Telefone == telefone);
+            Assert.IsTrue(estabelecimento.Categorias == listaCategorias);
+            Assert.IsTrue(estabelecimento.HorarioAbertura == horaAbertura);
+            Assert.IsTrue(estabelecimento.HorarioFechamento == horaFechamento);
+            Assert.IsTrue(estabelecimento.PrecoMedio == precoMedio);
+            Assert.IsTrue(estabelecimento.Fotos == listaFotos);
+            Assert.IsTrue(estabelecimento.Aprovado == aprovado);
+            Assert.IsTrue(estabelecimento.ValidarEntrada().Count == 1);
+            Assert.IsTrue(estabelecimento.ValidarEntrada().Contains("Telefone não pode ser nulo."));
+        }
+
+        [TestMethod]
+        public void Testar_Validar_De_Estabelecimento_Com_Telefone_Maior()
+        {
+            Endereco endereco = new Endereco("rua teste", "numero teste", "", "", "Centro", "Porto Alegre", "Rio Grande do Sul", "96508060", 15.0M, 15.0M);
+            List<Categoria> listaCategorias = new List<Categoria>();
+            List<Foto> listaFotos = new List<Foto>();
+            Estabelecimento estabelecimento;
+            var nome = "nome teste";
+            var telefone = "+5551987654321982236511";
+            DateTime horaFechamento = new DateTime().Date;
+            DateTime horaAbertura = new DateTime().Date;
+            decimal precoMedio = 14.0M;
+            bool aprovado = true;
+
+            estabelecimento = new Estabelecimento(nome, telefone, endereco, listaCategorias, horaAbertura, horaFechamento, precoMedio, listaFotos, aprovado);
+
+            Assert.IsTrue(estabelecimento.Nome == nome);
+            Assert.IsTrue(estabelecimento.Telefone == telefone);
+            Assert.IsTrue(estabelecimento.Categorias == listaCategorias);
+            Assert.IsTrue(estabelecimento.HorarioAbertura == horaAbertura);
+            Assert.IsTrue(estabelecimento.HorarioFechamento == horaFechamento);
+            Assert.IsTrue(estabelecimento.PrecoMedio == precoMedio);
+            Assert.IsTrue(estabelecimento.Fotos == listaFotos);
+            Assert.IsTrue(estabelecimento.Aprovado == aprovado);
+            Assert.IsTrue(estabelecimento.ValidarEntrada().Count == 1);
+            Assert.IsTrue(estabelecimento.ValidarEntrada().Contains("Tamanho máximo 20 caracteres."));
+        }
+
+        [TestMethod]
+        public void Testar_Validar_De_Estabelecimento_Com_Preco_Medio_Nulo_Ou_Vazio()
+        {
+            Endereco endereco = new Endereco("rua teste", "numero teste", "", "", "Centro", "Porto Alegre", "Rio Grande do Sul", "96508060", 15.0M, 15.0M);
+            List<Categoria> listaCategorias = new List<Categoria>();
+            List<Foto> listaFotos = new List<Foto>();
+            Estabelecimento estabelecimento;
+            var nome = "nome teste";
+            var telefone = "telefone teste";
+            DateTime horaFechamento = new DateTime().Date;
+            DateTime horaAbertura = new DateTime().Date;
+            decimal precoMedio = 0M;
+            bool aprovado = true;
+
+            estabelecimento = new Estabelecimento(nome, telefone, endereco, listaCategorias, horaAbertura, horaFechamento, precoMedio, listaFotos, aprovado);
+
+            Assert.IsTrue(estabelecimento.Nome == nome);
+            Assert.IsTrue(estabelecimento.Telefone == telefone);
+            Assert.IsTrue(estabelecimento.Categorias == listaCategorias);
+            Assert.IsTrue(estabelecimento.HorarioAbertura == horaAbertura);
+            Assert.IsTrue(estabelecimento.HorarioFechamento == horaFechamento);
+            Assert.IsTrue(estabelecimento.PrecoMedio == precoMedio);
+            Assert.IsTrue(estabelecimento.Fotos == listaFotos);
+            Assert.IsTrue(estabelecimento.Aprovado == aprovado);
+            Assert.IsTrue(estabelecimento.ValidarEntrada().Count == 1);
+            Assert.IsTrue(estabelecimento.ValidarEntrada().Contains("O Estabelecimento deve conter um preço médio."));
+        }
+
+        [TestMethod]
+        public void Testar_Validar_De_Estabelecimento_Com_Lista_De_Fotos_Nulo_Ou_Vazio()
+        {
+            Endereco endereco = new Endereco("rua teste", "numero teste", "", "", "Centro", "Porto Alegre", "Rio Grande do Sul", "96508060", 15.0M, 15.0M);
+            List<Categoria> listaCategorias = new List<Categoria>();
+            List<Foto> listaFotos = null;
+            Estabelecimento estabelecimento;
+            var nome = "nome teste";
+            var telefone = "telefone teste";
+            DateTime horaFechamento = new DateTime().Date;
+            DateTime horaAbertura = new DateTime().Date;
+            decimal precoMedio = 15.4M;
+            bool aprovado = true;
+
+            estabelecimento = new Estabelecimento(nome, telefone, endereco, listaCategorias, horaAbertura, horaFechamento, precoMedio, listaFotos, aprovado);
+
+            Assert.IsTrue(estabelecimento.Nome == nome);
+            Assert.IsTrue(estabelecimento.Telefone == telefone);
+            Assert.IsTrue(estabelecimento.Categorias == listaCategorias);
+            Assert.IsTrue(estabelecimento.HorarioAbertura == horaAbertura);
+            Assert.IsTrue(estabelecimento.HorarioFechamento == horaFechamento);
+            Assert.IsTrue(estabelecimento.PrecoMedio == precoMedio);
+            Assert.IsTrue(estabelecimento.Fotos == listaFotos);
+            Assert.IsTrue(estabelecimento.Aprovado == aprovado);
+            Assert.IsTrue(estabelecimento.ValidarEntrada().Count == 1);
+            Assert.IsTrue(estabelecimento.ValidarEntrada().Contains("A Lista de Fotos não pode ser nula."));
+        }
+
+        [TestMethod]
+        public void Testar_Validar_De_Estabelecimento_Com_Lista_De_Categorias_Nulo_Ou_Vazio()
+        {
+            Endereco endereco = new Endereco("rua teste", "numero teste", "", "", "Centro", "Porto Alegre", "Rio Grande do Sul", "96508060", 15.0M, 15.0M);
+            List<Categoria> listaCategorias = null;
+            List<Foto> listaFotos = new List<Foto>();
+            Estabelecimento estabelecimento;
+            var nome = "nome teste";
+            var telefone = "telefone teste";
+            DateTime horaFechamento = new DateTime().Date;
+            DateTime horaAbertura = new DateTime().Date;
+            decimal precoMedio = 15.4M;
+            bool aprovado = true;
+
+            estabelecimento = new Estabelecimento(nome, telefone, endereco, listaCategorias, horaAbertura, horaFechamento, precoMedio, listaFotos, aprovado);
+
+            Assert.IsTrue(estabelecimento.Nome == nome);
+            Assert.IsTrue(estabelecimento.Telefone == telefone);
+            Assert.IsTrue(estabelecimento.Categorias == listaCategorias);
+            Assert.IsTrue(estabelecimento.HorarioAbertura == horaAbertura);
+            Assert.IsTrue(estabelecimento.HorarioFechamento == horaFechamento);
+            Assert.IsTrue(estabelecimento.PrecoMedio == precoMedio);
+            Assert.IsTrue(estabelecimento.Fotos == listaFotos);
+            Assert.IsTrue(estabelecimento.Aprovado == aprovado);
+            Assert.IsTrue(estabelecimento.ValidarEntrada().Count == 1);
+            Assert.IsTrue(estabelecimento.ValidarEntrada().Contains("A Lista de Categorias não pode ser nula."));
+        }
+
+        [TestMethod]
+        public void Testar_Validar_De_Estabelecimento_Metodo_Esta_Aberto_Return_True()
+        {
+            Endereco endereco = new Endereco("rua teste", "numero teste", "", "", "Centro", "Porto Alegre", "Rio Grande do Sul", "96508060", 15.0M, 15.0M);
+            List<Categoria> listaCategorias = null;
+            List<Foto> listaFotos = new List<Foto>();
+            Estabelecimento estabelecimento;
+            var nome = "nome teste";
+            var telefone = "telefone teste";
+            DateTime horaFechamento = new DateTime().Date.AddHours(3);
+            DateTime horaAbertura = new DateTime().Date;
+            decimal precoMedio = 15.4M;
+            bool aprovado = true;
+
+            estabelecimento = new Estabelecimento(nome, telefone, endereco, listaCategorias, horaAbertura, horaFechamento, precoMedio, listaFotos, aprovado);
+
+            Assert.IsTrue(estabelecimento.Nome == nome);
+            Assert.IsTrue(estabelecimento.Telefone == telefone);
+            Assert.IsTrue(estabelecimento.Categorias == listaCategorias);
+            Assert.IsTrue(estabelecimento.HorarioAbertura == horaAbertura);
+            Assert.IsTrue(estabelecimento.HorarioFechamento == horaFechamento);
+            Assert.IsTrue(estabelecimento.PrecoMedio == precoMedio);
+            Assert.IsTrue(estabelecimento.Fotos == listaFotos);
+            Assert.IsTrue(estabelecimento.Aprovado == aprovado);
+            Assert.IsTrue(estabelecimento.EstaAberto(new DateTime().Date));
+        }
+
+        [TestMethod]
+        public void Testar_Validar_De_Estabelecimento_Metodo_Esta_Aberto_Return_False()
+        {
+            Endereco endereco = new Endereco("rua teste", "numero teste", "", "", "Centro", "Porto Alegre", "Rio Grande do Sul", "96508060", 15.0M, 15.0M);
+            List<Categoria> listaCategorias = null;
+            List<Foto> listaFotos = new List<Foto>();
+            Estabelecimento estabelecimento;
+            var nome = "nome teste";
+            var telefone = "telefone teste";
+            DateTime horaFechamento = new DateTime().Date.AddHours(12);
+            DateTime horaAbertura = new DateTime().Date.AddHours(3);
+            decimal precoMedio = 15.4M;
+            bool aprovado = true;
+
+            estabelecimento = new Estabelecimento(nome, telefone, endereco, listaCategorias, horaAbertura, horaFechamento, precoMedio, listaFotos, aprovado);
+
+            Assert.IsTrue(estabelecimento.Nome == nome);
+            Assert.IsTrue(estabelecimento.Telefone == telefone);
+            Assert.IsTrue(estabelecimento.Categorias == listaCategorias);
+            Assert.IsTrue(estabelecimento.HorarioAbertura == horaAbertura);
+            Assert.IsTrue(estabelecimento.HorarioFechamento == horaFechamento);
+            Assert.IsTrue(estabelecimento.PrecoMedio == precoMedio);
+            Assert.IsTrue(estabelecimento.Fotos == listaFotos);
+            Assert.IsTrue(estabelecimento.Aprovado == aprovado);
+            Assert.IsFalse(estabelecimento.EstaAberto(new DateTime().Date));
+        }
+
+
+        [TestMethod]
+        public void Testar_Validar_De_Estabelecimento_Metodo_Distancia_Estabelecimento()
+        {
+            Endereco endereco = new Endereco("rua teste", "numero teste", "", "", "Centro", "Porto Alegre", "Rio Grande do Sul", "96508060", 15.0M, 15.0M);
+            List<Categoria> listaCategorias = null;
+            List<Foto> listaFotos = new List<Foto>();
+            Estabelecimento estabelecimento;
+            var nome = "nome teste";
+            var telefone = "telefone teste";
+            DateTime horaFechamento = new DateTime().Date.AddHours(12);
+            DateTime horaAbertura = new DateTime().Date.AddHours(3);
+            decimal precoMedio = 15.4M;
+            bool aprovado = true;
+            var latitude = 100M;
+            var longitude = 100M;
+
+            estabelecimento = new Estabelecimento(nome, telefone, endereco, listaCategorias, horaAbertura, horaFechamento, precoMedio, listaFotos, aprovado);
+      
+            estabelecimento.DistanciaEstabelecimento(latitude, longitude);
+            var result = (decimal)Math.Sqrt((Math.Pow((double)(latitude - estabelecimento.Endereco.Latitude), 2.0)) + (Math.Pow((double)(longitude - estabelecimento.Endereco.Longitude), 2.0)));
+
+            Assert.IsTrue(estabelecimento.Nome == nome);
+            Assert.IsTrue(estabelecimento.Telefone == telefone);
+            Assert.IsTrue(estabelecimento.Categorias == listaCategorias);
+            Assert.IsTrue(estabelecimento.HorarioAbertura == horaAbertura);
+            Assert.IsTrue(estabelecimento.HorarioFechamento == horaFechamento);
+            Assert.IsTrue(estabelecimento.PrecoMedio == precoMedio);
+            Assert.IsTrue(estabelecimento.Fotos == listaFotos);
+            Assert.IsTrue(estabelecimento.Aprovado == aprovado);
+            Assert.IsTrue(estabelecimento.DistanciaEstabelecimento(latitude, longitude)==result);
         }
 
 
