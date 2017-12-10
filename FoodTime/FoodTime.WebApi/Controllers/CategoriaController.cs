3a8330a74d4337b98bd83def6e84849d31bcd66c
Vitor Staevie Ramos <vitor.ramos@cwi.com.br>
2017-12-10 17:30:35 -0200

    categoria controller adicionada para listar as categorias

---
+++ FoodTime/FoodTime.WebApi/Controllers/CategoriaController.cs
@@ -0,0 +1,42 @@
+﻿using FoodTime.WebApi.Models;
+using System.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FoodTime.Infraestrutura;
+using FoodTime.Dominio.Entidades;
+
+namespace FoodTime.WebApi.Controllers
+{
+
+
+    [AllowAnonymous]
+    [RoutePrefix("api/categoria")]
+    public class CategoriaController : ApiController
+    {
+        IFoodTimeContext context;
+
+        [HttpGet]
+        [Route("{id}")]
+        public IHttpActionResult BuscarCategoriaPorId([FromUri]int id)
+        {
+            Categoria categoria = context.Categorias.FirstOrDefault(x => x.Id == id);
+            if (categoria == null)
+                return BadRequest("Nenhuma categoria encontrada.");
+            return Ok(categoria);
+        }
+
+        [HttpGet]
+        [Route("listar")]
+        public IHttpActionResult BuscarTodasCategorias()
+        {
+            List<Categoria> ListaDeCategoria = context.Categorias.ToList();
+            if (ListaDeCategoria.Count == 0)
+                return BadRequest("Nenhuma categoria encontrada.");
+            return Ok(ListaDeCategoria);
+        }
+    }
+}
---
+++ FoodTime/FoodTime.WebApi/Controllers/FotoController.cs
@@ -0,0 +1,53 @@
+﻿using FoodTime.Dominio.Entidades;
+using FoodTime.Infraestrutura;
+using FoodTime.WebApi.Models;
+using System.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web;
+
+namespace FoodTime.WebApi.Controllers
+{
+    public class FotoController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult AdicionarFoto(HttpPostedFileBase file)
+        {
+
+            if (file.ContentLength > 0 && file != null)
+            {
+
+                //        model.ANEXO.NOME_ARQUIVO = file.FileName;
+                //        model.ANEXO.TIPO_CONTENT = file.ContentType;
+                //        model.ANEXO.NOME_HASH = model.ID_ATIVIDADE_EXTENSAO + Criptografia.MD5Hash(model.ANEXO.NOME_ARQUIVO);
+
+                //        var fileNameHash = Path.GetFileName(model.ANEXO.NOME_HASH);
+                //        var fileName = Path.GetFileName(model.ANEXO.NOME_ARQUIVO);
+                //        var directory = (String.IsNullOrEmpty(ConfigurationManager.AppSettings["pathAnexos"])) ? Server.MapPath("~/App_Data/Images/") : ConfigurationManager.AppSettings["pathAnexos"];
+
+                //        if (!Directory.Exists(directory))
+                //        {
+                //            Directory.CreateDirectory(directory);
+                //        }
+                //        var path = Path.Combine(directory, fileNameHash);
+                //        var pathOriginal = Path.Combine(directory, fileName);
+                //        model.ANEXO.TIPO_ARQUIVO = Path.GetExtension(pathOriginal);
+                //        file.SaveAs(path);
+
+                //        model.ANEXO.ID_ATIVIDADE_EXTENSAO = model.ID_ATIVIDADE_EXTENSAO;
+                //        model.ANEXO.Insert();
+                //        TempData["Message_Anexo"] = "Arquivo armazenado com sucesso!";
+
+                //    return null;
+                //}
+
+            }
+            return null;
+
+        }
+    }
+}
