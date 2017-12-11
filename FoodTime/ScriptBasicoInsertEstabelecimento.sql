50412a0fadf4779aa01ff9f742c04a125e3e63ae
Mathias Voelcker <mathias_V94@hotmail.com>
2017-12-11 13:35:07 -0200

    atualziar scripts de sql

--- FoodTime/ScriptBasicoInsertEstabelecimento.sql
+++ FoodTime/ScriptBasicoInsertEstabelecimento.sql
@@ -21,7 +21,7 @@ set comentario = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed d
 where id < 12;
 
 insert into schemaFoodTime.endereco
-values ('teste', 'teste', 'teste', 'teste', 'teste', 'teste', 'teste', 'teste', 5.0, 5.0);
+values ('teste', 'teste', 'teste', 'teste', 'teste', 'teste', 'teste', 'teste', -30, -50.0);
 insert into schemaFoodTime.endereco
 values ('teste', 'teste', 'teste', 'teste', 'teste', 'teste', 'teste', 'teste', 1.0, 1.0);
 insert into schemaFoodTime.endereco
@@ -59,14 +59,87 @@ values ('Super Restaurante', '92929832', CURRENT_TIMESTAMP , CURRENT_TIMESTAMP, 
 insert into schemaFoodTime.estabelecimento (nome, telefone, horarioAbertura, horarioFechamento, precoMedio, id_endereco)
 values ('Super Restaurante', '92929832', CURRENT_TIMESTAMP , CURRENT_TIMESTAMP, 65.2, 1);
 
-
-
-
-
 insert into schemaFoodTime.usuario (email, senha, nome, sobrenome, dataNascimento, fotoPerfil, admin)
 values ('teste', 'test', 'test', 'test', CURRENT_TIMESTAMP, 'test', 1);
 insert into schemaFoodTime.usuario (email, senha, nome, sobrenome, dataNascimento, fotoPerfil, admin)
 values ('teste1', 'teste1', 'teste1', 'teste1', CURRENT_TIMESTAMP, 'teste1', 0);
+
+insert into schemaFoodTime.usuario (email, senha, nome, sobrenome, dataNascimento, fotoPerfil, admin)
+values ('teste2', 'teste1', 'teste1', 'teste1', CURRENT_TIMESTAMP, 'teste1', 0);
+
+update schemaFoodTime.estabelecimento
+set id_endereco = 11
+where id = 3;
+
+update schemaFoodTime.usuario
+set FotoPerfil = ''
+where id < 6;
+
+
+insert into schemaFoodTime.endereco
+values ('teste', 'teste', 'teste', 'teste', 'teste', 'teste', 'teste', 'teste', -30, -51.0);
+insert into schemaFoodTime.endereco
+values ('teste', 'teste', 'teste', 'teste', 'teste', 'teste', 'teste', 'teste', -30.1, -51.1);
+insert into schemaFoodTime.endereco
+values ('teste', 'teste', 'teste', 'teste', 'teste', 'teste', 'teste', 'teste', -30.2, -51.1);
+insert into schemaFoodTime.endereco
+values ('teste', 'teste', 'teste', 'teste', 'teste', 'teste', 'teste', 'teste', -30.0, -51.1);
+insert into schemaFoodTime.endereco
+values ('teste', 'teste', 'teste', 'teste', 'teste', 'teste', 'teste', 'teste', -30.2, -51.0);
+insert into schemaFoodTime.endereco
+values ('teste', 'teste', 'teste', 'teste', 'teste', 'teste', 'teste', 'teste', -30.1, -51.2);
+insert into schemaFoodTime.endereco
+values ('teste', 'teste', 'teste', 'teste', 'teste', 'teste', 'teste', 'teste', -30.0, -51.2);
+insert into schemaFoodTime.endereco
+values ('teste', 'teste', 'teste', 'teste', 'teste', 'teste', 'teste', 'teste', -30.1, -51.15);
+
+select * from schemaFoodTime.categoria;
+
+insert into schemaFoodTime.preferencia
+values ('Pizza', 1);
+insert into schemaFoodTime.preferencia
+values ('Japonesa', 1);
+insert into schemaFoodTime.preferencia
+values ('Hamburgueria', 1);
+insert into schemaFoodTime.preferencia
+values ('Saudavel', 1);
+insert into schemaFoodTime.preferencia
+values ('Fast Food', 1);
+insert into schemaFoodTime.preferencia
+values ('Trash', 0);
+
+
+insert into schemaFoodTime.Categoria
+values ('Cafe');
+insert into schemaFoodTime.Categoria
+values ('Vida Noturna');
+insert into schemaFoodTime.Categoria
+values ('Almoço/Jantar');
+
+update schemaFoodTime.usuario
+set email = 'mathias@gmail.com'
+where id = 1;
+
+insert into schemaFoodTime.Foto (caminho, Id_Estabelecimzento) 
+values('https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Mcdonalds-90s-logo.svg/220px-Mcdonalds-90s-logo.svg.png',1)
+insert into schemaFoodTime.Foto (caminho, Id_Estabelecimzento) 
+values('https://s3.amazonaws.com/media.cuponeria.com.br/2017/07/cupom-outback-_2421-420x315.jpg',2)
+insert into schemaFoodTime.Foto (caminho,Id_Estabelecimzento) 
+values('http://pizzariafratello.com.br/wp-content/uploads/2015/09/logo_fratello.png',3)
+insert into schemaFoodTime.Foto (caminho, Id_Estabelecimzento) 
+values('https://www.temakeriajapesca.com.br/pedidos/image/data/temakeriajapesca-logo.png',4)
+insert into schemaFoodTime.Foto (caminho, Id_Estabelecimzento) 
+values('http://tibumbr.com.br/storage/empresasgallery/888/1449550011122033488868131746128784723704758n.jpg',5)
+insert into schemaFoodTime.Foto (caminho, Id_Estabelecimzento)
+values('https://i2.wp.com/www.grubgrade.com/wp-content/uploads/2017/10/02346-01_FarmhouseKing-Homepage-Banner_1800x760-CR.jpg?resize=500%2C418',6)
+insert into schemaFoodTime.Foto (caminho, Id_Estabelecimzento) 
+values('http://plugcitarios.com/wp-content/uploads/2016/04/Habibs.png',7)
+insert into schemaFoodTime.Foto (caminho, Id_Estabelecimzento) 
+values('https://coisasqueeugostodotcom1.files.wordpress.com/2013/02/le-grand-burger5.jpg',8)
+insert into schemaFoodTime.Foto (caminho, Id_Estabelecimzento)
+values('http://peti-app.ddns.net:8070/deliveryWS2/webresources/Delivery/storeimage/1',9)
+insert into schemaFoodTime.Foto (caminho, Id_Estabelecimzento) 
+values('http://www.sebraemercados.com.br/wp-content/uploads/2017/05/restaurante-sebrae-2-e1494853241838.jpg',10) 
 
 select pref.descricao
 from schemaFoodTime.preferencia pref inner JOIN
