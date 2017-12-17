0546f27665aa3a7522435d678fce699a5bedae33
Mathias <mathias_V94@hotmail.com>
2017-12-17 12:26:39 -0200

    ajustar merge

--- FrontEndFoodTime/usuario/perfilUsuario.controller.js
+++ FrontEndFoodTime/usuario/perfilUsuario.controller.js
@@ -1,76 +1,76 @@
 angular.module('app')
-  .controller('PerfilUsuarioController', function ($scope, $routeParams, authService, $http, usuarioService) {
+.controller('PerfilUsuarioController', function ($scope, $routeParams, authService, $http, usuarioService) {
 
-    $scope.usuario = authService.getUsuario();
-    console.log($scope.usuario)
-    // console.log($scope.usuario.DataNascimento)
-    // console.log($scope.usuario)
-    buscarRecomendacoes()
-    $scope.carregouRecomendacao = false
+  $scope.usuario = authService.getUsuario();
+  console.log($scope.usuario)
+  // console.log($scope.usuario.DataNascimento)
+  // console.log($scope.usuario)
+  buscarRecomendacoes()
+  $scope.carregouRecomendacao = false
 
 
-    usuarioService.buscarAvaliacoesUsuario($scope.usuario.Id).then(
-      function (response) {
-        console.log(response.data)
-        $scope.avaliacoes = response.data
-        $scope.$apply()
+  usuarioService.buscarAvaliacoesUsuario($scope.usuario.Id).then(
+    function (response) {
+      console.log(response.data)
+      $scope.avaliacoes = response.data
+      $scope.$apply()
+    }
+  )
+
+  $scope.retornarPreferencias = function(){
+    var preferencias = ""
+    $scope.usuario.Preferencias.forEach(function(preferencia) {
+      preferencias = preferencias + preferencia.Descricao + ", ";
+    })
+    return preferencias.substring(0, preferencias.length - 2);
+  }
+
+  // //teste de buscar o primeiro
+  // buscarUsuario(1);
+
+  function buscarRecomendacoes() {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
       }
-    )
-
-    $scope.getStars = function(rating) {
-      // Get the value
-      var val = parseFloat(rating);
-      // Turn value into number/100
-      var size = val*10;
-      return size + '%';
-    }
-
-    // //teste de buscar o primeiro
-    // buscarUsuario(1);
-
-    function buscarRecomendacoes() {
-      navigator.geolocation.getCurrentPosition(function (position) {
-        var pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        }
-        console.log(pos)
-        usuarioService.buscarRecomendacao($scope.usuario.Id, pos.lat, pos.lng).then(
-          function (response) {
-            console.log(response)
-            $scope.recomendacoes = response.data
-            $scope.carregouRecomendacao = true
-          }
-        )
-      })
-    }
-
-    $scope.excluirRecomendacao = function (idEstabelecimento) {
-      $scope.carregouRecomendacao = false
-      usuarioService.excluirRecomendacao(idEstabelecimento, $scope.usuario.Id).then(
+      console.log(pos)
+      usuarioService.buscarRecomendacao($scope.usuario.Id, pos.lat, pos.lng).then(
         function (response) {
           console.log(response)
-          buscarRecomendacoes()
+          $scope.recomendacoes = response.data
+          $scope.carregouRecomendacao = true
         }
       )
-    }
+    })
+  }
 
-    $scope.formatarPrecoMedio = function(numero){
-      numero = numero.toFixed(2);
-      return numero.toString().replace(/[.]/, ",")
-    }
+  $scope.excluirRecomendacao = function (idEstabelecimento) {
+    $scope.carregouRecomendacao = false
+    usuarioService.excluirRecomendacao(idEstabelecimento, $scope.usuario.Id).then(
+      function (response) {
+        console.log(response)
+        buscarRecomendacoes()
+      }
+    )
+  }
 
-    $scope.formatarData =function (data) {
-      data = data.substring(0, 10)
-      var dataArray = data.split("-")
-      var retorno = dataArray[2] + "/" + dataArray[1] + "/" + dataArray[0]
-      return retorno
-    }
+  $scope.formatarPrecoMedio = function(numero){
+    numero = numero.toFixed(2);
+    return numero.toString().replace(/[.]/, ",")
+  }
 
-    $scope.formatarData =function (data) {
-      data = data.substring(0, 10)
-      var dataArray = data.split("-")
-      var retorno = dataArray[2] + "/" + dataArray[1] + "/" + dataArray[0]
-      return retorno
-    } 
-  });
+  $scope.formatarData =function (data) {
+    data = data.substring(0, 10)
+    var dataArray = data.split("-")
+    var retorno = dataArray[2] + "/" + dataArray[1] + "/" + dataArray[0]
+    return retorno
+  }
+
+  $scope.formatarData =function (data) {
+    data = data.substring(0, 10)
+    var dataArray = data.split("-")
+    var retorno = dataArray[2] + "/" + dataArray[1] + "/" + dataArray[0]
+    return retorno
+  }
+});
